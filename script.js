d05e3677e4323a237732835e5111a9bcdfe45708
mohammedad.work@gmail.com <mohammedad.work@gmail.com>
2024-12-15 01:54:04 +0300

    Update

--- script.js
+++ script.js
@@ -12,15 +12,16 @@ document.getElementById('taskForm').addEventListener('submit', function (e) {
     const taskList = document.getElementById('taskList');
     
     const row = document.createElement('tr');
-    row.className = 'child';
+    row.className = "child";
     row.innerHTML = `
     <td>${taskName}</td>
-    <td>${taskDescription || '-'}</td>
+    <td class="description">${taskDescription || '-'}</td>
     <td class="priority-${taskPriority}">${taskPriority.charAt(0).toUpperCase() + taskPriority.slice(1)}</td>
-    <td>${dueDate || 'No date'}</td>
+    <td>${dueDate || 'None'}</td>
     <td class="actions">
     <button class="edit" onclick="editTask(this)">Edit</button>
     <button onclick="deleteTask(this)">Delete</button>
+    <button class ="view" onclick="viewElement(this)">View</button>
     </td>
     `;
 
@@ -28,9 +29,23 @@ document.getElementById('taskForm').addEventListener('submit', function (e) {
     container.hidden = false; 
 
     taskList.appendChild(row);
-    console.log(row)
     this.reset();
 });
+
+// Function to close the pop-out window
+function closeWindow() {
+    const element = document.querySelector('.overlay-card').remove();
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
+
+const currentDate = new Date();
+
+const formattedDate = currentDate.toLocaleDateString('en-US', {
+    month: 'long',
+    year: 'numeric'
+});
+document.getElementById('current-date').textContent = formattedDate;
 
 function deleteTask(button) {
     const row = button.parentElement.parentElement;
@@ -41,7 +56,17 @@ function deleteTask(button) {
     }
 }
 
+function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+    if (document.querySelector('.material-icons').textContent == 'wb_sunny') {
+        document.querySelector('.material-icons').textContent = 'dark_mode';
+    }else {
+        document.querySelector('.material-icons').textContent = 'wb_sunny';
+    }
+}
+
 function editTask(button) {
+    console.log(button)
     const row = button.parentElement.parentElement;
     const taskName = row.children[0].textContent;
     const taskDescription = row.children[1].textContent;
@@ -51,7 +76,33 @@ function editTask(button) {
     document.getElementById('taskName').value = taskName;
     document.getElementById('taskDescription').value = taskDescription === '-' ? '' : taskDescription;
     document.getElementById('taskPriority').value = taskPriority;
-    document.getElementById('dueDate').value = dueDate === 'No date' ? '' : dueDate;
+    document.getElementById('dueDate').value = dueDate === 'None' ? '' : dueDate;
 
     row.remove();
+}
+
+function viewElement(button) {
+    console.log(button)
+    
+    const row = button.parentElement.parentElement;
+    const overlay = document.getElementById('overlay');
+    
+    const taskName = row.children[0].textContent;
+    const taskDescription = row.children[1].textContent;
+    const taskPriority = row.children[2].textContent.toLowerCase();
+    const dueDate = row.children[3].textContent;
+    const rowOverlay = document.createElement('div');
+    rowOverlay.className = 'overlay-card';
+
+    rowOverlay.innerHTML = `
+    <h1>${taskName}</h1>
+    <p>Description</p>
+    <span class = "overlay-card-span">${taskDescription}</span>
+    <p>Priority: <span class="priority-${taskPriority}">${taskPriority.charAt(0).toUpperCase() + taskPriority.slice(1)}</span></p>
+    <p>Due Date: <span>${dueDate}</span></p>
+    `;
+    
+    overlay.style.display = 'flex';
+
+    overlay.appendChild(rowOverlay);
 }
